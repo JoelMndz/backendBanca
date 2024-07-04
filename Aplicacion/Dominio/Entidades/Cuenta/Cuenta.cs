0815cabb98a0dcf96628d6214113fa7d8080488e
JoelMndz <luisjo3lml@gmail.com>
2024-07-03 22:03:19 -0500

    Agregar archivos de proyecto.

---
+++ Aplicacion/Dominio/Entidades/Cuenta/Cuenta.cs
@@ -0,0 +1,16 @@
+﻿using Aplicacion.Dominio.Comunes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Dominio.Entidades.Cuenta
+{
+    public class Cuenta:EntidadBase
+    {
+        public string Numero { get; set; } = string.Empty;
+        public double Saldo { get; set; }
+        public int IdUsuario { get; set; }
+    }
+}
---
+++ Aplicacion/Dominio/Entidades/Usuario/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using Aplicacion.Dominio.Comunes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Dominio.Entidades.Usuario
+{
+    public class Usuario : EntidadBase
+    {
+        public string Nombres { get; set; } = string.Empty;
+        public string Apellidos { get; set; } = string.Empty;
+        public string Cedula { get; set; } = string.Empty;
+        public string CodigoDactilar { get; set; } = string.Empty;
+        public string Celular { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Provincia { get; set; } = string.Empty;
+        public string FotoRostroURL { get; set; } = string.Empty;
+        public string SituacionLaboral { get; set; } = string.Empty;
+        public string? Empresa { get; set; }
+        public string PaisPagoImpuestos { get; set; } = string.Empty;
+        public bool AceptoTerminosYConcidiones { get; set; } = false;
+    }
+}
