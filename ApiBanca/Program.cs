b0286e1aff6ac8746ce15a741d458bdf06f6ecb0
JoelMndz <luisjo3lml@gmail.com>
2024-07-23 21:02:40 -0500

    Preprarar para produccion

--- ApiBanca/Program.cs
+++ ApiBanca/Program.cs
@@ -73,11 +73,9 @@ builder.Services.AddAplicacion(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthentication();
 app.UseAuthorization();
